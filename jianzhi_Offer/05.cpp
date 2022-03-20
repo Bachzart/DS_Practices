7dc5bab3ef79c96bc50a40d7c111f945a0de61f5
bachzart <whistlesilp@gmail.com>
2022-03-20 22:52:29 +0800

    add new solutions

--- jianzhi_Offer/05.cpp
+++ jianzhi_Offer/05.cpp
@@ -1,3 +1,4 @@
+/* method 1 */
 class Solution {
 public:
     string replaceSpace(string s) {
@@ -7,5 +8,17 @@ public:
             else ret += s[i];
         }
         return ret;
+    }
+};
+
+/* method 2 */
+class Solution {
+public:
+    string replaceSpace(string s) {
+        string ret;
+        for(int i = 0; i < s.length(); i++) {
+            if(s[i] == ' ') s.replace(i, 1, "%20");
+        }
+        return s;
     }
 };
