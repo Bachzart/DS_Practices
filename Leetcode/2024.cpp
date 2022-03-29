91464143fdc3ba2273e567b6b8e49fcc201145e6
bachzart <whistlesilp@gmail.com>
2022-03-29 22:04:52 +0800

    daily task

---
+++ Leetcode/2024.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxConsecutive(string answerKey, int k, char ch) {
+        int n = answerKey.length(), ans = 0;
+        for(int left = 0, right = 0, sum = 0; right < n; right++) {
+            sum += answerKey[right] != ch;
+            while(sum > k) {
+                sum -= answerKey[left++] != ch;
+            }
+            ans = max(ans, right - left + 1);
+        }
+        return ans;
+    }
+    int maxConsecutiveAnswers(string answerKey, int k) {
+        return max(maxConsecutive(answerKey, k, 'T'), maxConsecutive(answerKey, k, 'F'));
+    }
+};
