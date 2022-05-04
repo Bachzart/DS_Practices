7b364c035cdc3676b83d33730efc75012f276e6a
bachzart <whistlesilp@gmail.com>
2022-05-04 23:46:55 +0800

    day4

---
+++ Leetcode/1367.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+    bool dfs(TreeNode *rt, ListNode *head) {
+        if(head == nullptr) return true;
+        if(rt == nullptr) return false;
+        if(rt->val != head->val) return false;
+        return dfs(rt->left, head->next) || dfs(rt->right, head->next);
+    }
+public:
+    bool isSubPath(ListNode* head, TreeNode* root) {
+        if(root == nullptr) return false;
+        return dfs(root, head) || isSubPath(head, root->left) || isSubPath(head, root->right);
+    }
+};
