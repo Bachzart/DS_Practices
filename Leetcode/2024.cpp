ddded8846fea4581775c6a32a33e7f45aaafb4aa
bachzart <whistlesilp@gmail.com>
2022-06-11 23:54:51 +0800

    add new solution

--- Leetcode/2024.cpp
+++ Leetcode/2024.cpp
@@ -15,3 +15,22 @@ public:
         return max(maxConsecutive(answerKey, k, 'T'), maxConsecutive(answerKey, k, 'F'));
     }
 };
+
+/* use lambda function */
+class Solution {
+public:
+    int maxConsecutiveAnswers(string answerKey, int k) {
+        auto func = [&](char ch) -> int {
+            int n = answerKey.length(), ret = 0;
+            for(int left = 0, right = 0, sum = 0; right < n; right++) {
+                sum += answerKey[right] != ch;
+                while(sum > k) {
+                    sum -= answerKey[left++] != ch;
+                }
+                ret = max(ret, right - left + 1);
+            }
+            return ret;
+        };
+        return max(func('T'), func('F'));
+    }
+};
