a76a801ec21e1d0f465adcbad2a4bd9008ec9e37
bachzart <whistlesilp@gmail.com>
2022-06-21 21:04:28 +0800

    daily task

---
+++ Leetcode/1108.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string ans = "";
+        for(char ch: address) {
+            if(ch == '.') ans += "[.]";
+            else ans.push_back(ch);
+        }
+        return ans;
+    }
+};
