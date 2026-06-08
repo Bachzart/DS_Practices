711f457f937c065503bb92e542944c0bf419835c
bachzart <whistlesilp@gmail.com>
2026-06-08 22:37:14 +0800

    add new solution

--- Leetcode/2196.cpp
+++ Leetcode/2196.cpp
@@ -9,6 +9,36 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/*
+Method 2: hash + bit operation
+*/
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        unordered_map<int, TreeNode*> nodeMap;
+        nodeMap.reserve(descriptions.size() + 1);
+        int root = 0;
+        for(auto& v: descriptions) {
+            int p = v[0], c = v[1], isLeft = v[2];
+            if(!nodeMap.count(p)) {
+                nodeMap[p] = new TreeNode(p);
+                root ^= p;
+            }
+            if(!nodeMap.count(c)) {
+                nodeMap[c] = new TreeNode(c);
+                root ^= c;
+            }
+
+            isLeft ? nodeMap[p]->left = nodeMap[c] : nodeMap[p]->right = nodeMap[c];
+            root ^= c;
+        }
+        return nodeMap[root];
+    }
+};
+
+/*
+Method 1: double hash
+
 class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
@@ -31,3 +61,5 @@ public:
         return nullptr;
     }
 };
+
+*/
