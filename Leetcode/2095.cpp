c090c53481bf5d50a0109fa7fb35f6e6e2702bf4
bachzart <whistlesilp@gmail.com>
2026-06-15 11:37:37 +0800

    daily task

---
+++ Leetcode/2095.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if(head->next == nullptr) {
+            return nullptr;
+        }
+        ListNode* pre = nullptr;
+        auto slow = head, fast = head;
+        while(fast && fast->next) {
+            pre = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        pre->next = slow->next;
+        delete slow;
+        return head;
+    }
+};
