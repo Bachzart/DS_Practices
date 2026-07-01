f2f969a2756b33020aa3e6479941653782f8d6f2
bachzart <whistlesilp@gmail.com>
2026-07-01 20:00:16 +0800

    add new solutions

--- Leetcode/1358.cpp
+++ Leetcode/1358.cpp
@@ -1,3 +1,27 @@
+/*
+Method 1: Sliding Window, fix the right point and slide the left point.
+*/
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int ht[3] = {0};
+        int left = 0, ans = 0;
+        for(char c: s) {
+            ht[c - 'a']++;
+            while(ht[0] && ht[1] && ht[2]) {
+                ht[s[left] - 'a']--;
+                left++;
+            }
+            ans += left;
+        }
+
+        return ans;
+    }
+};
+
+/*
+Method 2: Sliding Window, fix the left point and slide the right point.
+
 class Solution {
 public:
     int numberOfSubstrings(string s) {
@@ -15,3 +39,4 @@ public:
         return ans;
     }
 };
+*/
--- Leetcode/1846.cpp
+++ Leetcode/1846.cpp
@@ -1,3 +1,26 @@
+/*
+Method 2: Counting Sort
+*/
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> cnt(n + 1);
+        for(int x: arr) {
+            cnt[min(x, n)]++;
+        }
+
+        int ans = 0;
+        for(int x = 1; x <= n; ++x) {
+            ans = min(ans + cnt[x], x);
+        }
+        return ans;
+    }
+};
+
+/*
+Method 1: Greedy + Sort
+
 class Solution {
 public:
     int maximumElementAfterDecrementingAndRearranging(vector<int> &arr) {
@@ -10,3 +33,5 @@ public:
         return arr.back();
     }
 };
+
+*/
